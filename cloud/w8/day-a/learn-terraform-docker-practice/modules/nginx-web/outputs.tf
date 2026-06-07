66baf7e6edc7e5053c8760ac2accb755d195ba16
Van Phu Tin <vanphutin.contact@gmail.com>
2026-06-07 14:00:18 +0700

    refactor: remove deprecated Terraform configuration and Nginx module files

--- cloud/w8/day-a/learn-terraform-docker-practice/modules/nginx-web/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "container_id" {
-description = "ID của Docker container được tạo"
-  value       = docker_container.nginx.id
-}
-
-output "website_url" {
-  description = "Đường dẫn URL để truy cập vào container"
-  value       = "http://localhost:${var.host_port}"
-}
--- cloud/w8/day-a/learn-terraform-docker-practice/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "website_urls" {
-  description = "Danh sách URL truy cập vào các trang web đã deploy"
-  value       = { for k, v in module.nginx_websites : k => v.website_url }
-}
